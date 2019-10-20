6041bca8e94342222b147c15db3a61feae96c595
Mehmet Veysel Sekendiz <mehmetveyselsekendiz@gmail.com>
2019-10-20 22:04:02 +0300

    This application is created for finding the biggest number from 3 numbers.

---
+++ src/tr/mvs/tutorial/java/BigestNumber.java
@@ -0,0 +1,34 @@
+package tr.mvs.tutorial.java;
+
+import java.util.Scanner;
+// Mehmet Veysel Sekendiz 20195156004 //
+
+public class BigestNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three number for comperison: ");
+        double n1 = input.nextDouble();
+        double n2 = input.nextDouble();
+        double n3 = input.nextDouble();
+
+        if (n1 > n2)
+        {
+
+            if (n1 > n3) {
+                System.out.print(n1 + " is the biggest number");
+            }
+            else {
+                System.out.print(n3 + " is the biggest number");
+            }
+        }
+        else if (n2 > n3)
+        {
+            System.out.print(n2 + " is the biggest number");
+        }
+        else{
+            System.out.print(n3+ " is the biggest number");
+
+
+        }
+    }
+}
